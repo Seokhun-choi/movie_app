aa61debb8f2144cd4f6cb77ef097af6f409efe40
seokhun <xocoolree@gmail.com>
2021-01-11 20:39:30 +0900

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Movie from "./Movie";
 
-function App() {
+class App extends React.Component {
+  state ={
+    isLoading = true;
+    movies =[]
+  };
+
+  
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie />
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Movie() {
+  return <div>한국말로는 안녕하세요.</div>;
+}
+
+export default Movie;
